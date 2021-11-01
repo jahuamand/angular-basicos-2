9e476017b42b1957247ecf0c6b597fea1fa24744
Jorge Huaman <jahuamand@gmail.com>
2021-10-31 20:04:31 -0500

    Primer commit -v2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+//import { ContadorComponent } from './contador/contador/contador.component';
+
+// Tarea:
+// Crear un modulo llamado ContadorModule
+// declaraciones y exportaciones
+import { ContadorModule } from './contador/contador.module';
+
+// import { HeroeComponent } from './heroes/heroe/heroe.component';
+// import { ListadoComponent } from './heroes/listado/listado.component';
+import { HeroesModule } from './heroes/heroes.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    //ContadorComponent
+    // HeroeComponent,
+    // ListadoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
